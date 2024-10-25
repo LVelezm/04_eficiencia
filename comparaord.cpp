68bdfe2503c49c68a6819d69a0f268ef507701a5
lvelezm <lvelezm@unjbg.edu.pe>
2024-10-25 12:12:44 -0500

    Se hicieron cambios para que los numeros aleatorios no se repitan y que tuviese un contador de comparaciones

--- comparaord.cpp
+++ comparaord.cpp
@@ -3,12 +3,14 @@
 #include <cstdlib>
 #include <chrono>
 using namespace std;
-void SeleccionDir(int A[], int n) {
+void SeleccionDir(int A[], int n, long long& comparaciones) {
+	comparaciones=0;
     for (int i = 0; i < n - 1; i++) {
         int menor = A[i];
         int k = i;
 
         for (int j = i + 1; j < n; j++) {
+        	comparaciones++;
             if (menor > A[j]) {
                 menor = A[j];
                 k = j;
@@ -19,11 +21,13 @@ void SeleccionDir(int A[], int n) {
         A[i] = menor;
     }
 }
-void InsercionDir(int A[], int n) {
+void InsercionDir(int A[], int n, long long& comparaciones) {
+	comparaciones=0;
     for (int i = 1; i < n; i++) {
         int aux = A[i];
         int k = i - 1;
         while (k >= 0 && aux < A[k]) {
+        	comparaciones++;
             A[k + 1] = A[k];
             k = k - 1;
         }
@@ -34,14 +38,25 @@ int main(){
 	srand(time(0));
 	int n;
 	int opcion;
+	long long comparaciones;
 	cout<<"Ingrese la cantidad de numeros aleatorios a generar(preferiblemente de 10000 a mas): ";
 	cin>>n;
 	int arreglo[n];
 	int copia[n];
-	for(int i=0; i<n; i++){
-		arreglo[i]=rand()%2*n+1-n;
-		copia[i]=arreglo[i];	
-	}
+	for (int i = 0; i < n; i++) {
+        arreglo[i] = i + 1;
+    }
+
+    for (int i = 0; i < n; i++) {
+        int j = rand() % n;
+        int temp = arreglo[i];
+        arreglo[i] = arreglo[j];
+        arreglo[j] = temp;
+    }
+
+    for (int i = 0; i < n; i++) {
+        copia[i] = arreglo[i];
+    }
 	do{
 		cout<<"Elija una opcion: "<<endl;
 		cout<<"1) Ordenacion por seleccion"<<endl;
@@ -51,16 +66,18 @@ int main(){
 		cin>>opcion;
 		if(opcion==1){
 			auto inicioSeleccion=std::chrono::high_resolution_clock::now();
-			SeleccionDir(arreglo, n);
+			SeleccionDir(arreglo, n, comparaciones);
 			auto finSeleccion=std::chrono::high_resolution_clock::now();
 			auto duracionSeleccion=std::chrono::duration_cast<std::chrono::milliseconds>(finSeleccion-inicioSeleccion).count();
 			cout<<"Tiempo de ordenacion por seleccion: "<<duracionSeleccion<<"ms"<<endl;
+			cout<<"Comparaciones realizadas "<<comparaciones<<endl;
 		}else if(opcion==2){	
 			auto inicioInsercion=std::chrono::high_resolution_clock::now();
-			InsercionDir(copia, n);
+			InsercionDir(copia, n, comparaciones);
 			auto finInsercion=std::chrono::high_resolution_clock::now();
 			auto duracionInsercion=std::chrono::duration_cast<std::chrono::milliseconds>(finInsercion-inicioInsercion).count();
 			cout<<"Tiempo de ordenacion por insercion: "<<duracionInsercion<<"ms"<<endl;
+			cout<<"Comparaciones realizadas "<<comparaciones<<endl;
 		}else if(opcion!=3){
 			cout<<"opcion invalida"<<endl;
 		}
